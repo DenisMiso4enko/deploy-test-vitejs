4823ca8b4f1da60b1204164779a594ecc9860b2b
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-12-01 16:48:20 +0300

    feat: add rootReducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import {useDispatch, useSelector} from "react-redux";
 
 function App() {
     const dispatch = useDispatch()
-    const cash = useSelector(state => state.cash)
+    const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
+
 
     const addCash = (cash) => {
         dispatch({type: "ADD_CASH", payload: cash})
@@ -11,11 +13,36 @@ function App() {
     const getCash = (cash) => {
         dispatch({type: "GET_CASH", payload: cash})
     }
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now()
+        }
+        dispatch({type: "ADD_CUSTOMER", payload: customer})
+    }
+    const removeCustomer = (customer) => {
+        dispatch({type: "REMOVE_CUSTOMER", payload: customer.id})
+    }
     return (
         <div className="App">
             <h1>{cash}</h1>
-            <button onClick={() => addCash(Number(prompt()))}>Пополнить</button>
-            <button onClick={() => getCash(Number(prompt()))}>Снять</button>
+            <button onClick={() => addCash(50)}>Пополнить</button>
+            <button onClick={() => getCash(5)}>Снять</button>
+            <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+
+            {customers.map(customer => {
+                return <div key={customer.id} onClick={() => removeCustomer(customer)}>{customer.name}</div>
+            })}
+
+
+            {/*{customers.length ?*/}
+            {/*    (<div>*/}
+            {/*        {customers.map(customer =>*/}
+            {/*        <div key={customer.id} onClick={() => removeCustomer(customer)}>*/}
+            {/*            <p key={customer.id}>{customer.name}</p>*/}
+            {/*        </div>)}*/}
+            {/*    </div>):*/}
+            {/*    (<div>Нет клиентов</div>)}*/}
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,15 @@
- import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
- import {createStore} from "redux";
- import {Provider} from "react-redux";
+import {Provider} from "react-redux";
+import {store} from "./store/index.js";
 
-const defaultState = {
-    cash: 0
-}
-
-const reducer = (state = defaultState, action) => {
-    switch (action.type) {
-        case "ADD_CASH": {
-            return {
-                ...state,
-                cash: state.cash + action.payload
-            }
-        }
-        case "GET_CASH": {
-            return {
-                ...state,
-                cash: state.cash -  action.payload
-            }
-        }
-        default: return state
-
-    }
-}
-
- const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App/>
-      </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 )
