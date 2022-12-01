6fa0d48fa6e197bf285967c8bd41f6e17b1492a1
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-12-01 15:37:37 +0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+
+function App() {
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+    return (
+        <div className="App">
+            <h1>{cash}</h1>
+            <button onClick={() => addCash(Number(prompt()))}>Пополнить</button>
+            <button onClick={() => getCash(Number(prompt()))}>Снять</button>
+        </div>
+    )
+}
+
+export default App
